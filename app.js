3094a03be1a7b5ed6c51a0f0c8847c8eb36b3fa0
multiservismalaga-cpu <multiservismalaga@gmail.com>
2026-09-24 18:48:43 +0200

    Mostrar cruce VIN Hyundai

--- app.js
+++ app.js
@@ -194,7 +194,7 @@ function renderVinDecode(data) {
   const rows = (data.results || []).map(x =>
     '<div class="vin-row"><b>' + esc(x.field) + '</b><span>' + esc(x.value) + '</span></div>'
   ).join("");
-  const hxHtml = hx ? "<section class=\"vin-crosscheck\">VIN cruzado</section>" : "";
+  let hxHtml = hx ? "<section class=\"vin-crosscheck\"><div class=\"eyebrow\">CRUCE VIN</div><h3>" + esc(hx[0]) + " " + esc(hx[1]) + " " + esc(hx[2]) + "</h3>" : "";
   box.innerHTML =
     '<div class="detail-nav"><div class="nav-crumb">Identificación VIN</div><button id="closeVin" class="secondary">Cerrar</button></div>' +
     '<div class="eyebrow">IDENTIFICACIÓN POR VIN</div><h3>VIN ' + esc(data.vin) + '</h3>' + hxHtml +
